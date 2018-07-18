5ede6209c81e8e232c1b7678abb59c890023641d
“Tyler <tylerpalef@gmail.com>
2018-07-18 16:25:58 -0400

    finished exercise

---
+++ grades.rb
@@ -0,0 +1,13 @@
+puts "please provide a percentage"
+percentage = gets.chomp.to_s
+
+if percentage <= "50%"
+  puts "you have failed"
+
+elsif percentage >= "70%"
+  puts "you have a B"
+
+elsif percentage >= "80%"
+  puts "you have an A!"
+
+end
