0501950f730372543d378093b695a1782bbb5568
Billy Davidson <billy.davids52@gmail.com>
2018-07-20 13:39:14 -0400

    grades

--- grades.rb
+++ grades.rb
@@ -1,15 +1,21 @@
-puts "please provide a percentage"
-percentage = gets.chomp.to_s
+def get_grade(percent)
+  puts "please provide a number/percentage"
+  percent = gets.chomp.to_i
 
-if percentage <= "50%"
-  puts "you have failed"
+  if percent <= 50
+    puts "you have failed"
 
-elsif percentage >= "70%"
-  puts "you have a B"
+  elsif percent >= 51 && percent <= 69
+    puts "you got a D ;)"
 
-elsif percentage >= "80%"
-  puts "you have an A!"
+  elsif percent >= 70
+    puts "you have a B"
 
+  elsif percent >= 80
+    puts "you have an A!"
+
+  end
 end
-
+percent = "nil"
+puts get_grade(percent)
 # excel
